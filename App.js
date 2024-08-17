6e43a48704a21d9ef09d727895f00bca6516db8b
Emisael Kisler <kisler.fullstack@gmail.com>
2024-08-17 19:53:52 -0400

    part10-6 OK

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { NativeRouter } from "react-router-native";
+import { StatusBar } from "expo-status-bar";
 import Main from "./src/components/Main";
 
 const App = () => {
   return (
     <>
+    <StatusBar style="light" />
+    <NativeRouter>
       <Main />
+    </NativeRouter>
     </>
   );
 };
