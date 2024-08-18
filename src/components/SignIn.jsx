7034955d188e8ab6a3b55dad03ca23b16b96b03f
Emisael Kisler <kisler.fullstack@gmail.com>
2024-08-18 11:40:12 -0400

    part10-8 OK

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,63 @@
 import React from "react";
-import Text from "./Text";
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import { Formik } from "formik";
+import FormikTextInput from "./FormikTextInput";
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const styles = StyleSheet.create({
+    allMargin: {
+      margin: 12,
+    },
+    customButton: {
+      height: 65,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#007bff",
+      borderRadius: 5,
+    },
+    buttonText: {
+      fontSize: 18,
+      color: "#ffffff",
+      fontWeight: "bold",
+    },
+  });
+
+  return (
+    <View>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View style={styles.allMargin}>
+            <FormikTextInput
+              name="username"
+              placeholder="Username"
+              onChangeText={handleChange("username")}
+              onBlur={handleBlur("username")}
+              value={values.username}
+            />
+            <FormikTextInput
+              name="password"
+              placeholder="Password"
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+              secureTextEntry
+            />
+            <TouchableOpacity
+              style={styles.customButton}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.buttonText}>Sign in</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
